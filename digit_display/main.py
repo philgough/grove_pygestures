94ab555fa05d940cd124bffaef5d6c68f0c9e7e6
Phil Gough <phil.j.gough@gmail.com>
2019-09-20 12:46:52 +1000

    update main.py for digit_display example

--- digit_display/main.py
+++ digit_display/main.py
@@ -2,16 +2,22 @@ from microbit import *
 from digit_display import *
 
 display.show(Image.HAPPY)
+# initialise the display at pin 15/1
 grove_display = Grove4DigitDisplay(15, 1)
 t = 1234
 count = 0
-print('hello, world')
+sleep(400)
 
 while True:
-
+    # show the current value of t to the display
     grove_display.show(t)
+
+    # flash the colon on the digital display
     grove_display.set_colon(count & 1)
+
+    # update value of t
     t += 1
+    # update value of counter (to flash the colon on the digital display)
     count += 1
-    print(t)
+
     sleep(50)
